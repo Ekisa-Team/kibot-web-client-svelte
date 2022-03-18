b49e1f2d618d75120febebc4c498c403748734a9
juanmesa2097 <juanmesa2097@gmail.com>
2022-03-17 22:35:29 -0500

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,27 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<section>
+	<h1>
+		<div class="welcome">
+			<picture>
+				<source srcset="svelte-welcome.webp" type="image/webp" />
+				<img src="svelte-welcome.png" alt="Welcome" />
+			</picture>
+		</div>
+
+		to your new<br />SvelteKit app
+	</h1>
+
+	<h2>
+		try editing <strong>src/routes/index.svelte</strong>
+	</h2>
+</section>
---
+++ src/routes/messaging-providers/index.svelte
@@ -0,0 +1,8 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Messaging providers</title>
+</svelte:head>
+
+<div />
