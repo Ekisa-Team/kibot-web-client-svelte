8e8691f656af23b51fd361444efc61782da26216
juanmesa2097 <juanmesa2097@gmail.com>
2022-03-19 01:28:27 -0500

    Add storage utilities

---
+++ src/lib/utils/local-storage.ts
@@ -0,0 +1,23 @@
+export enum LocalStorageItem {
+	SidebarStatus = 'app::sidebar::status'
+}
+
+function getItem<T>(item: LocalStorageItem, shouldParse = true): T | null {
+	const data = localStorage.getItem(item.toString()) || '{}';
+	return data && shouldParse ? JSON.parse(data) : data;
+}
+
+function setItem<T>(item: LocalStorageItem, data: T): void {
+	if (data === null || data === undefined) return;
+	localStorage.setItem(item.toString(), JSON.stringify(data));
+}
+
+function removeItem(item: LocalStorageItem): void {
+	localStorage.removeItem(item.toString());
+}
+
+function clear(): void {
+	localStorage.clear();
+}
+
+export { getItem, setItem, removeItem, clear };
---
+++ src/lib/utils/session-storage.ts
@@ -0,0 +1,23 @@
+export type SessionStorageItem = {
+	SidebarStatus: 'app::sidebar::status';
+};
+
+function getItem<T>(item: SessionStorageItem, shouldParse = true): T | null {
+	const data = sessionStorage.getItem(item.toString()) || '{}';
+	return data && shouldParse ? JSON.parse(data || '{}') : data;
+}
+
+function setItem<T>(item: SessionStorageItem, data: T): void {
+	if (data === null || data === undefined) return;
+	sessionStorage.setItem(item.toString(), JSON.stringify(data));
+}
+
+function removeItem(item: SessionStorageItem): void {
+	sessionStorage.removeItem(item.toString());
+}
+
+function clear(): void {
+	localStorage.clear();
+}
+
+export { getItem, setItem, removeItem, clear };
