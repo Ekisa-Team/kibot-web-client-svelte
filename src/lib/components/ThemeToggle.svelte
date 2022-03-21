517a626377129bc0d9895e27e5c6710ee61df62f
juanmesa2097 <juanmesa2097@gmail.com>
2022-03-21 00:34:39 -0500

    Refactor theme toggle component

--- src/lib/components/ThemeToggle.svelte
+++ src/lib/components/ThemeToggle.svelte
@@ -1,19 +1,48 @@
 <script lang="ts">
   import { Theme } from '$lib/enums/theme';
+  import { getTheme } from '$lib/services/theme';
   import { appTheme } from '$lib/stores/app-theme';
   import { t } from '$lib/translations';
-  import { Menu, MenuButton, MenuItem, MenuItems, Transition } from '@rgossiaux/svelte-headlessui';
+  import {
+    Listbox,
+    ListboxButton,
+    ListboxOption,
+    ListboxOptions,
+    Transition
+  } from '@rgossiaux/svelte-headlessui';
 
-  const handleThemeChange = (theme: import('$lib/enums/theme').Theme) => {
-    appTheme.update(() => theme);
+  const themes = [
+    {
+      value: Theme.Light,
+      text: $t('layout.navbar.themePicker.light'),
+      icon: 'i-carbon:light text-xl'
+    },
+    {
+      value: Theme.Dark,
+      text: $t('layout.navbar.themePicker.dark'),
+      icon: 'i-carbon:moon text-xl'
+    },
+    {
+      value: Theme.System,
+      text: $t('layout.navbar.themePicker.system'),
+      icon: 'i-carbon:screen text-xl'
+    }
+  ];
+
+  let selectedTheme = themes.find((t) => t.value === getTheme()) || themes[0];
+
+  const handleThemeChange = (event: CustomEvent<any>) => {
+    selectedTheme = event.detail;
+    appTheme.update(() => selectedTheme.value);
   };
 </script>
 
-<Menu let:open class="relative">
+<Listbox value={selectedTheme} on:change={handleThemeChange} let:open class="relative">
   <!-- menu button -->
-  <MenuButton class="btn btn-default">
-    <div class="i-carbon:screen text-xl" />
-  </MenuButton>
+  <ListboxButton class="btn btn-default">
+    <div class={selectedTheme.icon} />
+    <span class="ml-2">{selectedTheme.text}</span>
+  </ListboxButton>
 
   <!-- transition -->
   <Transition
@@ -25,37 +54,25 @@
     leaveFrom="transform scale-100 opacity-100"
     leaveTo="transform scale-95 opacity-0">
     <!-- menu items -->
-    <MenuItems class="dropdown dropdown-bottom-left w-40">
-      <MenuItem let:active>
-        <button
-          href="/account-settings"
-          class="dropdown-item whitespace-nowrap"
-          class:dropdown-item-active={active}
-          on:click={() => handleThemeChange(Theme.Light)}>
-          <div class="i-carbon:light text-xl" />
-          <span class="ml-3 text-sm font-semibold">{$t('layout.navbar.themePicker.light')}</span>
-        </button>
-      </MenuItem>
-      <MenuItem let:active>
-        <button
-          href="/account-settings"
-          class="dropdown-item whitespace-nowrap"
-          class:dropdown-item-active={active}
-          on:click={() => handleThemeChange(Theme.Dark)}>
-          <div class="i-carbon:moon text-xl" />
-          <span class="ml-3 text-sm font-semibold">{$t('layout.navbar.themePicker.dark')}</span>
-        </button>
-      </MenuItem>
-      <MenuItem let:active>
-        <button
-          href="/account-settings"
-          class="dropdown-item whitespace-nowrap"
-          class:dropdown-item-active={active}
-          on:click={() => handleThemeChange(Theme.System)}>
-          <div class="i-carbon:screen text-xl" />
-          <span class="ml-3 text-sm font-semibold">{$t('layout.navbar.themePicker.system')}</span>
-        </button>
-      </MenuItem>
-    </MenuItems>
+    <ListboxOptions class="dropdown dropdown-bottom-left w-40">
+      {#each themes as theme}
+        <ListboxOption value={theme} let:active let:selected>
+          <div class="dropdown-item whitespace-nowrap" class:active class:selected>
+            <div class={theme.icon} />
+            <span class="ml-3 text-sm font-semibold">{theme.text}</span>
+          </div>
+        </ListboxOption>
+      {/each}
+    </ListboxOptions>
   </Transition>
-</Menu>
+</Listbox>
+
+<style>
+  .active {
+    @apply bg-secondary;
+  }
+  .selected {
+    @apply bg-secondary;
+    @apply text-blue-500;
+  }
+</style>
